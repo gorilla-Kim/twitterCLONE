1f4aeca86c225cfbfbb71861a7b086313d1bdef7
gorilla-Kim <00ghks22@naver.com>
2020-08-09 00:49:49 +0900

    antd 를 이용한 네브바 디자인

--- front/components/AppLayout.js
+++ front/components/AppLayout.js
@@ -1,29 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import Link from 'next/link'
+import { Input, Menu, Row, Col } from 'antd'
+import LoginForm from '../components/LoginForm'
+import UserProfile from '../components/UserProfile'
 
+// gutter 는 컬럼사이의 간격(컬럼사이의 padding이 추가됨)
 const AppLayout = ({ children }) => {
-    return (
-        <div>
-            <div>
-                <Link href="/">
-                    <a>노드버드</a>
-                </Link>
-                <Link href={'/profile'}>
-                    <a>프로필</a>
-                </Link>
-                <Link href={'/signup'}>
-                    <a>회원가입</a>
-                </Link>
-            </div>
-            <div>MAIN MENU</div>
-            {children}
-        </div>
-    )
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+  return (
+    <div>
+      <Menu mode="horizontal">
+        <Menu.Item>
+          <Link href="/">
+            <a>노드버드</a>
+          </Link>
+        </Menu.Item>
+        <Menu.Item>
+          <Link href={'/profile'}>
+            <a>프로필</a>
+          </Link>
+        </Menu.Item>
+        <Menu.Item>
+          <Input.Search enterButton style={{ verticalAlign: 'middle' }} />
+        </Menu.Item>
+        <Menu.Item>
+          <Link href={'/signup'}>
+            <a>회원가입</a>
+          </Link>
+        </Menu.Item>
+      </Menu>
+      <Row gutter={8}>
+        <Col xs={24} md={6}>
+          {isLoggedIn ? <UserProfile /> : <LoginForm />}
+        </Col>
+        <Col xs={24} md={12}>
+          {children}
+        </Col>
+        <Col xs={24} md={6}>
+          <a
+            href="https://www.naver.com"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            NAVER
+          </a>
+        </Col>
+      </Row>
+      {children}
+    </div>
+  )
 }
 
 AppLayout.propTypes = {
-    children: PropTypes.node.isRequired,
+  children: PropTypes.node.isRequired,
 }
 
 export default AppLayout
