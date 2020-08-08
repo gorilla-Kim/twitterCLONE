e6c1c6ffe857338e59727f4227470faf420cf671
gorilla-Kim <00ghks22@naver.com>
2020-08-09 00:51:15 +0900

    회원가입 폼 생성
    * antd를 이용

---
+++ front/components/LoginForm.js
@@ -0,0 +1,53 @@
+import React, { useState, useCallback } from 'react'
+import { Form, Input, Button } from 'antd'
+import Link from 'next/link'
+
+const LoginForm = () => {
+  const [id, setId] = useState('')
+  const [password, setPassword] = useState('')
+
+  const onchangeId = useCallback((e) => {
+    setId(e.target.value)
+  }, [])
+  const onchangePassword = useCallback((e) => {
+    setPassword(e.target.value)
+  }, [])
+  return (
+    <Form>
+      <div>
+        <label htmlFor="user-id">아이디</label>
+        <br />
+        <Input
+          name="user-id"
+          type="text"
+          value={id}
+          onChange={onchangeId}
+          required
+        />
+      </div>
+      <div>
+        <label htmlFor="user-password">비밀번호</label>
+        <br />
+        <Input
+          name="user-password"
+          type="password"
+          value={password}
+          onChange={onchangePassword}
+          required
+        />
+      </div>
+      <div>
+        <Button type="primary" htmlType="submit" loading={false}>
+          로그인
+        </Button>
+        <Link href={'/signup'}>
+          <a>
+            <Button>회원가입</Button>
+          </a>
+        </Link>
+      </div>
+    </Form>
+  )
+}
+
+export default LoginForm
