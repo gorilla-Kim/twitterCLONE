061f16b2e87c446d26ee40ad8295057873d17d6f
gorilla-Kim <00ghks22@naver.com>
2020-08-09 00:49:05 +0900

    html head테그 커스텀
    * next의 Head 컴포넌트를 이용

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -1,13 +1,23 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import Head from 'next/head'
 import 'antd/dist/antd.css'
 
+// html 에서 head테그에 접근하고 싶을때는 next의 Head 컴포넌트를 사용한다.
 const App = ({ Component }) => {
-    return <Component />
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <title>Node-Bird</title>
+      </Head>
+      <Component />
+    </>
+  )
 }
 
 App.propTypes = {
-    Component: PropTypes.elementType.isRequired,
+  Component: PropTypes.elementType.isRequired,
 }
 
 export default App
