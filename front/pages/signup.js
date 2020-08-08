40e70863c1860cac8f8a1dcca57623943a98c65c
gorilla-Kim <00ghks22@naver.com>
2020-08-09 00:50:22 +0900

    head테스의 title 값 추가
    * next의 Head 컴포넌트를 이용

--- front/pages/signup.js
+++ front/pages/signup.js
@@ -1,8 +1,17 @@
 import React from 'react'
 import AppLayout from '../components/AppLayout'
+import Head from 'next/head'
 
 const Signup = () => {
-    return <AppLayout>Signup</AppLayout>
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <title>회원가입 | Node-Bird</title>
+      </Head>
+      <AppLayout>Signup</AppLayout>
+    </>
+  )
 }
 
 export default Signup
